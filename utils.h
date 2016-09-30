905ccace1c891b85b7b05d5e9c3a2d87575c4ed9
rongcan <rongcan@localhost.com>
2016-09-30 10:41:15 +0800

    fix a warning

--- utils.h
+++ utils.h
@@ -17,7 +17,7 @@ const char* time_now() {
   gettimeofday(&tv, NULL);
   
   snprintf(timestamp_str, sizeof(timestamp_str), 
-           "%d%02d%02d-%02d:%02d:%02d.%03d\n", 1900 + timeinfo->tm_year,
+           "%d%02d%02d-%02d:%02d:%02d.%03ld\n", 1900 + timeinfo->tm_year,
            1 + timeinfo->tm_mon, timeinfo->tm_mday, timeinfo->tm_hour,
            timeinfo->tm_min, timeinfo->tm_sec, tv.tv_usec / 1000);
   return timestamp_str;
