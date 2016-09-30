3d6e11b3cceae1fc18691a405ca3800d30b5cd1c
rongcan zhao <rongcan@local.com>
2016-09-30 10:37:27 +0800

    add utils.h

---
+++ utils.h
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/time.h>
+
+const char* time_now() {
+  static char timestamp_str[32];
+  time_t rawtime;
+  struct tm *timeinfo;
+  time(&rawtime);
+  timeinfo = localtime(&rawtime);
+  
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+  
+  snprintf(timestamp_str, sizeof(timestamp_str), 
+           "%d%02d%02d-%02d:%02d:%02d.%03d\n", 1900 + timeinfo->tm_year,
+           1 + timeinfo->tm_mon, timeinfo->tm_mday, timeinfo->tm_hour,
+           timeinfo->tm_min, timeinfo->tm_sec, tv.tv_usec / 1000);
+  return timestamp_str;
+}
+
